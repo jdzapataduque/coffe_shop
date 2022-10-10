dcc0c97f81d08e332bc3e28eec39c481f8a854d5
Juan Diego Zapata <jdzapataduque@gmail.com>
2022-10-09 23:48:17 -0500

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:coffe_shop/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -8,15 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Tienda del cafe'),
-        ),
-        body: const Center(
-          child: Text('Hola Juan Diego Zapata'),
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        home: LoginScreen());
   }
 }
