eecde765e97af0d9b505fc5b474cb16bf0c7548b
Juan Diego Zapata <jdzapataduque@gmail.com>
2022-10-10 00:05:00 -0500

    Text field del email solamente

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,10 +8,36 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String _email = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Login SCREEEENN')),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _showemail(),
+        ],
+      )),
+    );
+  }
+
+  Widget _showemail() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        autofocus: true,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'Email...',
+          labelText: 'Email',
+          suffixIcon: Icon(Icons.email),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        ),
+        onChanged: (value) {
+          _email = value;
+        },
+      ),
     );
   }
 }
