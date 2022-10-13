29c43d8d1a481245a5209c14c616a3b2db0d8f6c
Juan Diego Zapata <jdzapataduque@gmail.com>
2022-10-12 21:53:13 -0500

    Modify scale to 2.63 of logo.PNG

--- lib/screens/app_bar.dart
+++ lib/screens/app_bar.dart
@@ -2,59 +2,63 @@ import 'package:flutter/material.dart';
 import '../utils/color_utils.dart';
 import 'search.dart';
 import 'package:coffe_shop/screens/login_screen.dart';
-AppBar header(context,{bool isAppTitle=false,String strTitle="",disableBackButton=false}){
+
+AppBar header(context,
+    {bool isAppTitle = false,
+    String strTitle = "",
+    disableBackButton = false}) {
   return AppBar(
-      centerTitle: true,
-      title: Row(
+    centerTitle: true,
+    title: Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-      Image.asset(
-      'assets/img/logo.PNG',
-      scale: 2.5,
-  ),
-  const SizedBox(
-  width: 20,
-  ),
-  ],
-  ),
-  actions: <Widget>[
-  IconButton(
-  icon: const Icon(Icons.search_rounded),
-  tooltip: 'Buscar',
-  onPressed: () {
-  showSearch(context: context, delegate:MySearch() );
-  },
-  ),
-  IconButton(
-  icon: const Icon(Icons.account_circle_outlined),
-  tooltip: 'Login',
-  onPressed: () {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
-  },
-  ),
-  IconButton(
-  icon: const Icon(Icons.shopping_cart),
-  tooltip: 'Carrito de compras',
-  onPressed: () {
-  Navigator.push(context, MaterialPageRoute<void>(
-  builder: (BuildContext context) {
-  return Scaffold(
-  appBar: AppBar(
-  title: const Text('Next page'),
-  ),
-  body: const Center(
-  child: Text(
-  'This is the next page',
-  style: TextStyle(fontSize: 24),
-  ),
-  ),
+        Image.asset(
+          'assets/img/logo.PNG',
+          scale: 2.63,
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+      ],
+    ),
+    actions: <Widget>[
+      IconButton(
+        icon: const Icon(Icons.search_rounded),
+        tooltip: 'Buscar',
+        onPressed: () {
+          showSearch(context: context, delegate: MySearch());
+        },
+      ),
+      IconButton(
+        icon: const Icon(Icons.account_circle_outlined),
+        tooltip: 'Login',
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginScreen()));
+        },
+      ),
+      IconButton(
+        icon: const Icon(Icons.shopping_cart),
+        tooltip: 'Carrito de compras',
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute<void>(
+            builder: (BuildContext context) {
+              return Scaffold(
+                appBar: AppBar(
+                  title: const Text('Next page'),
+                ),
+                body: const Center(
+                  child: Text(
+                    'This is the next page',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+              );
+            },
+          ));
+        },
+      ),
+    ],
+    backgroundColor: ("#FF0474").toColor(), //<-- SEE HERE
   );
-  },
-  ));
-  },
-  ),
-  ],
-  backgroundColor: ("#FF0474").toColor(), //<-- SEE HERE
-  );
-
 }
