640b06373c3dfd47c2d2ad24d9de1ec8700da7d4
Mauricio <mamesa@bancolombia.com.co>
2022-11-03 14:39:46 -0500

    ajuste mapa

--- lib/screens/mapa.dart
+++ lib/screens/mapa.dart
@@ -1,3 +1,4 @@
+import 'package:coffe_shop/screens/app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(6.227623664931631, -75.57505450783523);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -23,11 +24,9 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
+        appBar: customAppBar(),
         body: GoogleMap(
+          markers: _stores(),
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
             target: _center,
@@ -36,5 +35,32 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
     );
+  }
+
+  Set<Marker> _stores() {
+    final Set<Marker> markers = new Set();
+    markers.add(Marker(
+      markerId: MarkerId('tienda1'),
+      position:
+          LatLng(6.229052843017938, -75.57080588886014), //position of marker
+      infoWindow: InfoWindow(
+        title: 'My Custom Title ',
+        snippet: 'My Custom Subtitle',
+      ),
+      icon: BitmapDescriptor.defaultMarker, //Icon for Marker
+    ));
+
+    markers.add(Marker(
+      markerId: MarkerId('tienda2'),
+      position:
+          LatLng(6.230140722258932, -75.5739708953855), //position of marker
+      infoWindow: InfoWindow(
+        title: 'My Custom Title ',
+        snippet: 'My Custom Subtitle',
+      ),
+      icon: BitmapDescriptor.defaultMarker, //Icon for Marker
+    ));
+
+    return markers;
   }
 }
