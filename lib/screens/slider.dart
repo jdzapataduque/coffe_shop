bd89778e974d156be8e4a646c798d959cb874e76
Juan Diego Zapata <jdzapataduque@gmail.com>
2022-11-09 20:48:15 -0500

    Te ayudamos

--- lib/screens/slider.dart
+++ lib/screens/slider.dart
@@ -6,7 +6,11 @@ final List<String> imgList = [
   'https://cdn.shopify.com/s/files/1/0272/2873/3504/files/diagonal-opt_360x.jpg',
   'https://cdn.shopify.com/s/files/1/0272/2873/3504/files/Screen_Shot_2022-10-18_at_8.57.18_PM_900x.png'
 ];
-final List<String> reList = ['¿No sabes que café cuál café elegir?  te ayudamos', 'La tienda del café', 'Un café para el sueño'];
+final List<String> reList = [
+  '¿No sabes que café cuál café elegir? Te ayudamos',
+  'La tienda del café',
+  'Un café para el sueño'
+];
 
 final List<Widget> imageSliders = imgList
     .map((item) => Container(
@@ -34,8 +38,13 @@ final List<Widget> imageSliders = imgList
                         ),
                         padding: EdgeInsets.symmetric(
                             vertical: 10.0, horizontal: 20.0),
-                        child: Text(reList[imgList.indexOf(item)] ,
-                          style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold,),
+                        child: Text(
+                          reList[imgList.indexOf(item)],
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
